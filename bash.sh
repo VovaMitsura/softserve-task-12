445c1107d14aa72b9cfdbec488409ed294c9d31e
Vova Mitsura <vovamitsura@gmail.com>
2023-11-15 21:06:47 +0200

    Feat: add bash script and github actions workflow

---
+++ bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Variables containing first and last name
+first_name="Vova"
+last_name="Mitsura"
+
+# Displaying the first and last name
+echo "First Name: $first_name"
+echo "Last Name: $last_name"
